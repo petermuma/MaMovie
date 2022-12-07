1c0382e5e063be2763e2c47b2698c8aa2f5819b1
Ey Muma <petermuma04@gmail.com>
2022-12-07 12:07:00 +0300

    website
    12/7/22

---
+++ first.js
@@ -0,0 +1,37 @@
+  //setTimeout (wakeUpUser, 5000);
+        //function wakeUpUser(){
+            alert("Leave your thoughts here");
+//}
+
+let slideIndex =0;
+showSlides(slideIndex);
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slideIndex++;
+    if (slideIndex > slides.length){slideIndex =1}
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+
+
+    function openRegister(){
+      card.style.transform = "rotateY(-180deg)";
+    }
+  
+    function openLogin(){
+      card.style.transform = "rotateY(0deg)";
+    }
+  
+  
+  }
