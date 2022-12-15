d22790584d4766e9acbc9b7c6b1c36bdb9b0fcff
Ey Muma <petermuma04@gmail.com>
2022-12-15 23:45:31 +0300

    Added JS File
    15/12/2022

--- first.js
+++ first.js
@@ -3,26 +3,26 @@
             alert("Leave your thoughts here");
 //}
 
-let slideIndex =0;
-showSlides(slideIndex);
-  
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slideIndex++;
-    if (slideIndex > slides.length){slideIndex =1}
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
 
 
     function openRegister(){
@@ -34,4 +34,4 @@ showSlides(slideIndex);
     }
   
   
-  }
+  
